582b66109dbbe39de6d92db9ef23ab9332f3f54f
Mohammad Abdullah <mohammadabdullah5341@gmail.com>
2026-06-23 21:38:52 -0700

    client

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Button } from './components/ui/button'
+
+
+const App = () => {
+  return (
+    <div className="px-10">App
+    <div>
+     <Button>Hello</Button>
+    </div>
+    </div>
+  )
+}
+
+export default App
